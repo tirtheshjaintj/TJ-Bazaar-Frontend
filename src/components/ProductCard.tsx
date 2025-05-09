f992c02e8179ed61ebd103cc45ead42ab3c0332f
Tirthesh Jain <tirtheshjaintj@gmail.com>
2025-05-09 20:05:22 +0530

    UI and Optimization

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -13,7 +13,7 @@ interface ProductCardProps {
   product: Product;
   index: number;
 }
-export const share = (product_id: any, product_name: any) => {
+export const share = async (product_id: any, product_name: any) => {
   const productUrl = `${window.location.origin}/product/${product_id}`;
   const shareText = `Check out this awesome TJ Bazaar product: ${product_name}!\nHere at \n► ${productUrl} ◄\n`;
   await navigator.clipboard.writeText(shareText);
